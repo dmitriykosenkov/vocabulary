071fab108e6a44a31bb7557c2e635cfbb83d82cc
dmitriykosenkov <dmkos23011991@gmail.com>
2023-01-25 23:04:22 +0200

    second commit

--- src/components/WordItem/WordItem.jsx
+++ src/components/WordItem/WordItem.jsx
@@ -5,7 +5,7 @@ const WordItem = ({item, deleteItem}) => {
    return (
       <li key={item.id} className={s.bodyItem}>
          <div className={s.textItem}>
-            {item.en} - {item.ukr}
+            <span>{item.en}</span> - {item.ukr}
          </div>
          <button className={s.buttonItem}>Edit</button>
          <button className={s.buttonItem} onClick={() => deleteItem(item.id)}>Delete</button>
